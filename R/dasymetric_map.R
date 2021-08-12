52a68ddde6e4e09ead573c6645dc8355fca44bd5
JaFro96 <jannis.froehlking@gmail.com>
2021-08-12 11:56:45 +0200

    add readme

--- R/dasymetric_map.R
+++ R/dasymetric_map.R
@@ -10,4 +10,9 @@
 #' @examples dasymetric_map(a,b,c)
 dasymetric_map <- function(x, ancillary_data, target_geom) {
   print("TODO!")
+  p = sf::st_point(0:1)
+  plot(p, pch = 16)
+  title("point")
+  box(col = 'grey')
+  return(p)
 }
