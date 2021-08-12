5f378b07a432283b98b663e79de60afa18459c16
JaFro96 <jannis.froehlking@gmail.com>
2021-08-12 11:37:10 +0200

    documentation

--- R/dasymetric_map.R
+++ R/dasymetric_map.R
@@ -1,3 +1,13 @@
-dasymetric_map <- function() {
+#' Create a dasymetric map
+#'
+#' @param x numeric
+#' @param ancillary_data sf
+#' @param target_geom sf
+#'
+#' @return
+#' @export
+#'
+#' @examples dasymetric_map(a,b,c)
+dasymetric_map <- function(x, ancillary_data, target_geom) {
   print("TODO!")
 }
