8ff7ca48cb4cb05612d0cf07e91d8e0169e63d4b
JaFro96 <jannis.froehlking@gmail.com>
2021-08-24 18:16:44 +0200

    add building footprints as ancillary dataset

---
+++ R/buildings.R
@@ -0,0 +1,11 @@
+#' This is data of the residential building footprints of Münster
+#'
+#' @source <https://opendata.stadt-muenster.de/dataset/geb%C3%A4udeumrisse>
+#' @name buildings
+#' @format A data frame with columns:
+#' \describe{
+#'  \item{gid}{ID of building.}
+#'  \item{funktion}{Type of the building.}
+#'  \item{geometry}{Geometry of the building.}
+#'  }
+"buildings"
